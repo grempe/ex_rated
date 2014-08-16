04588266b91d6f0aac0b781f29fae252498eee9b
Glenn Rempe <glenn@rempe.us>
2014-08-16 10:32:28 -0700

    Open the Kimono.

---
+++ README.md
@@ -0,0 +1,4 @@
+ExRated
+=======
+
+** TODO: Add description **
---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule ExRated.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :ex_rated,
+     version: "0.0.1",
+     elixir: "~> 0.15.1",
+     deps: deps]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type `mix help compile.app` for more information
+  def application do
+    [applications: [:logger],
+     mod: {ExRated, []}]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type `mix help deps` for more examples and options
+  defp deps do
+    []
+  end
+end
