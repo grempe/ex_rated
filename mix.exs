4962f37d6fe66a002ea6aaa16b798ed75ff9c710
Glenn Rempe <glenn@rempe.us>
2015-08-16 10:58:29 -0700

    Bump version 0.0.6

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExRated.Mixfile do
 
   def project do
     [app: :ex_rated,
-     version: "0.0.5",
+     version: "0.0.6",
      elixir: "~> 1.0.0",
      description: description,
      package: package,
