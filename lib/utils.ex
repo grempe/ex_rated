648f0dd4cd22b8de169d6e07d512270893a2a6a5
Mitchell Henke <mitchh23@gmail.com>
2015-08-16 11:38:40 -0500

    add utils for checking OTP version

---
+++ lib/utils.ex
@@ -0,0 +1,24 @@
+defmodule ExRated.Utils do
+  @moduledoc false
+
+  @doc """
+  Determines the current version of OTP running this node. The result is
+  cached for fast lookups in performance-sensitive functions.
+  ## Example
+      iex> rel = ExRated.Utils.get_otp_release
+      ...> '\#{rel}' == :erlang.system_info(:otp_release)
+      true
+  """
+  def get_otp_release do
+    case Process.get(:current_otp_release) do
+      nil ->
+        case ("#{:erlang.system_info(:otp_release)}" |> Integer.parse) do
+          {ver, _} when is_integer(ver) ->
+            Process.put(:current_otp_release, ver)
+            ver
+          _ ->
+        end
+      ver -> ver
+    end
+  end
+end
