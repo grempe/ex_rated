40a25f146ec475f80a594af96e76a7ee315ecc40
Glenn Rempe <glenn@rempe.us>
2016-03-15 11:28:48 -0700

    Tighten up included files to eliminate publish warnings.

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule ExRated.Mixfile do
 
   defp package do
     [# These are the default files included in the package
-     files: ["lib", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+     files: ["lib", "mix.exs", "README.md", "LICENSE"],
      maintainers: ["Glenn Rempe"],
      licenses: ["Apache 2.0"],
      links: %{"GitHub" => "https://github.com/grempe/ex_rated"}]
